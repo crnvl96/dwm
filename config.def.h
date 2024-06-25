73518e04facc255d94c1b7bf020c711d2f389407
Ádran Farias Carnavale <adran.carnavale@gmail.com>
2024-06-25 09:50:26 -0300

    refact: changes menu cmd

--- config.def.h
+++ config.def.h
@@ -55,8 +55,9 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+// static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "rofi", "-show", "drun", NULL };
+static const char *termcmd[]  = { "kitty", NULL };
 static const char *brightnessupcmd[]  = { "/home/crnvl96/.config/crnvl96/scripts/brightness", "--inc", NULL };
 static const char *brightnessdowncmd[]  = { "/home/crnvl96/.config/crnvl96/scripts/brightness", "--dec", NULL };
 static const char *audioraisecmd[]  = { "/home/crnvl96/.config/crnvl96/scripts/volume", "--inc", NULL };
